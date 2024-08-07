c38c7ea69c14d444a7e3934add552e404b92a98a
akhil <akhilme341@gmail.com>
2024-08-08 00:28:11 +0530

    APP

--- app/src/main/java/com/example/myschools/model/SchoolModel.kt
+++ app/src/main/java/com/example/myschools/model/SchoolModel.kt
@@ -1,12 +1,13 @@
 package com.example.myschools.model
 
 import com.example.myschools.data.SchoolInfoItem
+import com.example.myschools.repo.DataRepository
 import com.example.myschools.repo.DataRepositoryImpl
 
 
 class SchoolModel {
     private lateinit var schoolList: ArrayList<SchoolInfoItem>
-    private lateinit var dataRepository: DataRepositoryImpl
+    private lateinit var dataRepository: DataRepository
      fun getSchools():ArrayList<SchoolInfoItem> {
        schoolList=dataRepository.getSchoolData()
         return (schoolList)
